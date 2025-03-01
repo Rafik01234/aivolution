78150b3f81d8d5d3cb72b2f78a5e965415c4a46a
Rafik01234 <100259893+Rafik01234@users.noreply.github.com>
2025-03-01 20:33:32 +0500

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold">Добро пожаловать в AIvolution</h1>
-      <p>Платформа дистанционного обучения с ИИ</p>
-    </div>
-  );
-}
